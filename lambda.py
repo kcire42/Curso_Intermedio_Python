b6d37971a7aad6c67ecc554eb4a026344e194c5c
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-09-06 14:25:38 -0500

    funcion map

--- lambda.py
+++ lambda.py
@@ -16,3 +16,33 @@ saludo(hola)
 saludo(adios)
 palindromo = lambda palabra : palabra == palabra[::-1]
 print(palindromo('ana'))
+#Funcion Filter
+
+
+
+
+
+
+
+
+
+
+
+
+#Map
+#Hacer numeros elevados 
+#Filtra numeros pares y multiplicarlos por dos con list comprehensions
+numeros = [1,2,3,4,5,6,7,8,9,10]
+pares = [i for i in numeros if i % 2 ==0]
+pares_dobles = [i*2 for i in numeros if i % 2 ==0]
+print(pares)
+print(pares_dobles)
+#filtrar numeros para y elevarlos y multiplicarlos por dos con for
+dobles_for =[]
+for i in numeros:
+    if i % 2 ==0: 
+        dobles_for.append(i*2)
+print(dobles_for)
+#doblar numeros en una lista con map
+cuadrados = list(map(lambda x: x**2,numeros))
+print(cuadrados)
