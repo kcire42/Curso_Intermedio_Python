421a44daf108717a6452d8bc0f49548f944bd3dd
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-09-06 23:22:02 -0500

    se agrega filter

--- lambda.py
+++ lambda.py
@@ -3,6 +3,17 @@
 #Estructura lambda argumentos:expresion 
 #Solo se puede tener una sola linea de codigo 
 #palindromo = lambda string : string == string[::-1]
+#Nombre de la funcion lambda = lambda variable que se va a usar : condicion a aplicar en la variable 
+#como resultado va a regresar un true o false
+
+
+
+palindromo = lambda palabra : palabra == palabra[::-1]
+print(palindromo('ana'))
+suma = lambda  y,z : y+z
+print(suma(2,3))
+
+#filter
 #Funcion de orden superior es una funcion que recive como parametro otra funcion y ejecuta una tarea 
 #EJEMPLO FUNCION DE ORDEN SUPERIOR
 def saludo(funcion):
@@ -11,8 +22,9 @@ def hola():
     print("Hola")
 def adios():
     print("adios")
-
 saludo(hola)
 saludo(adios)
-palindromo = lambda palabra : palabra == palabra[::-1]
-print(palindromo('ana'))
+numeros = [1,2,3,4,5,6,7,8,9,10]
+#La funcion filter se usa para filtra una lista de codigo
+pares = list(filter(lambda x: x%2 !=0,numeros))
+print(pares) 
---
+++ list_comprehensions.py
@@ -0,0 +1,24 @@
+
+'''Lista comprehension'''
+def main():
+    cuadrado = []
+    #for i in range(1,101):
+        #lista_pequeña = [i,i**2]
+        #cuadrado.append(lista_pequeña)
+    #     lista_dos = [i,i**2]
+    #     if i%3 == 0:
+    #         cuadrado.append(lista_dos)
+    # print(cuadrado)
+    #list comprehensions
+    lista = [ i for i in range (1,10000) if i % 4 == 0 and i % 6 == 0 and i % 9 == 0 ]
+    print(lista)
+    numeros = [1,2,3,4,5,6,7,8,9,10]
+    pares = [i for i in numeros if i % 2 == 0 ]
+    print(pares)
+    elevados_doble = [i for i in pares i*2 ]
+    print(elevados_doble)
+
+
+
+if __name__ == '__main__':
+    main()
