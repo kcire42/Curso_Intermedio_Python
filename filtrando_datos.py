8113a38f7f06b749aa9dd70f7a8a385769312fa3
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-09-08 23:28:00 -0500

    Se agrega filtro de personas

---
+++ filtrando_datos.py
@@ -0,0 +1,84 @@
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 15,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+def main():
+    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python" ]
+    print(all_python_devs)
+    all_platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    print(all_platzi_workers)
+    adultos_list = [worker["name"] for worker in DATA if worker["age"] >= 18]
+    print(adultos_list)
+    adultos_filter = list(filter(lambda))
+
+
+if __name__ == "__main__":
+    main()
