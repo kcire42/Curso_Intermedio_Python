306ef40d0e1fb69d20e359ee8b7f8026f6936842
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-09-14 05:58:10 -0500

    try catch

---
+++ try_catch.py
@@ -0,0 +1,16 @@
+def main():
+    try:
+        numero = int(input("Ingrese el numero positivo"))
+        print(numero)
+        if numero > 0:
+            print(numero)
+        if numero
+    except ValueError:
+        print("no acepto letras")
+         
+
+    
+
+
+if __name__ == '__main__':
+    main()
