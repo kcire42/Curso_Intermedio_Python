e936467cb8a3acbf002065e3b6d684b84a681264
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-09-14 19:59:10 -0500

    se agrega try catch final

--- try_catch.py
+++ try_catch.py
@@ -1,14 +1,21 @@
 def main():
     try:
         numero = int(input("Ingrese el numero positivo"))
-        print(numero)
-        if numero > 0:
-            print(numero)
-        if numero
+        #print(numero)
+        try:
+            if numero > 0:
+                print(f"EL numero es positivo y es: {numero}")
+        
+            elif  numero < 0:
+                raise ValueError("No se pueden colocar numero negativos")
+
+        except ValueError as positivo:
+            print(positivo)
+    
     except ValueError:
         print("no acepto letras")
-         
-
+    
+    
     
 
 
