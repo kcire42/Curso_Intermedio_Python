06c5a58b9641b57f23580f11b513836322805e87
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-09-19 15:35:15 -0500

    se agrega rutina assert

---
+++ assert.py
@@ -0,0 +1,19 @@
+def main():
+    numero = input("Ingrese el numero positivo: ")
+    assert  numero.isnumeric(), "Solo puedo agregar numeros"
+    
+    if int(numero) < 0:
+        assert unumero > 0, "no se pueden colocar numeros negativos"
+    else:
+        print(f"el numero es correcto: {numero}")
+            
+
+    
+        
+       
+                
+    
+
+
+if __name__ == '__main__':
+    main()
